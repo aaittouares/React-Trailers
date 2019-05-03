c9bd53ea92e64cb4a516b2cd3df51022c7afa4f6
alexandre aittouares <alex.aittouares@gmail.com>
2019-05-03 14:32:54 +0200

    chap 12

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const App = function(){
-    return <div>Salut têtes de nases</div>
-};
+import App from './containers/app'
+
+const API_KEY = 'f35fd0b9c4d5513a8779aabe2c0f5ca0';
+
 
 ReactDom.render(<App/>, document.querySelector('.container'));
