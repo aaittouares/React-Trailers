a6d768810f995b1ed0d3006ef4c3ca9df160c56f
alexandre aittouares <alex.aittouares@gmail.com>
2019-05-03 15:38:05 +0200

    chap 15

--- src/containers/video-list.js
+++ src/containers/video-list.js
@@ -9,7 +9,7 @@ const VideoList = () => {
             <ul>
                 {
                     movies.map( movie => {
-                        return <VideoListItem movie = {movie}/>
+                        return <VideoListItem key={movie} movie={movie}/>
                     })
                 }
             </ul>
