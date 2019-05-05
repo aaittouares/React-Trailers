35bf65678e5ce78199d48d9040d38244422b3116
alexandre aittouares <alex.aittouares@gmail.com>
2019-05-05 23:47:38 +0200

    chap 25

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -4,7 +4,12 @@ class SearchBar extends Component {
 
     constructor(props){
         super(props);
-        this.state = {searchText: '', placeHolder:'Tapez votre film svp ....'};
+        this.state = {
+            searchText: '',
+            placeHolder:'Tapez votre film svp ....',
+            intervalBeforeRequest: 1000,
+            lockRequest: false    
+        };
     }
 
     render(){
@@ -22,10 +27,20 @@ class SearchBar extends Component {
 
     handleChange(event){
         this.setState({searchText: event.target.value});
+        if(!this.state.lockRequest){
+            this.setState({lockRequest: true});
+            setTimeout(function(){this.search()}.bind(this), this.state.intervalBeforeRequest);
+        }
     }
 
     handleOnClick(event){
+        this.search();
+        
+    }
+
+    search(){
         this.props.callback(this.state.searchText);
+        this.setState({lockRequest: false});
     }
 }
 
